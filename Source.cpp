891cb470b58fc836d3279297786f7b94925a294a
lonelyuber <35495442+lonelyuber@users.noreply.github.com>
2018-01-19 08:10:00 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,12 @@
+#include <iostream>
 
+using namespace std;
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+int main() {
+
+	int age;
+	cout << "How old are you?" << endl;
+	cin >> age;
+	cout << "you are " << age << " years old. that's wack" << endl;
+
+}
